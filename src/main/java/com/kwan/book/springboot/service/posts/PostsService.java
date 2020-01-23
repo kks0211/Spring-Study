6ff201d6c663fcdfc665ea629150c3207c4ee170
Kwan <kks0211@hanmail.net>
2020-01-23 21:04:50 +0900

    게시글 저장, 수정, 삭제

--- src/main/java/com/kwan/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/kwan/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.kwan.book.springboot.service.posts;
 
 import com.kwan.book.springboot.web.domain.posts.Posts;
 import com.kwan.book.springboot.web.domain.posts.PostsRepository;
+import com.kwan.book.springboot.web.dto.PostsListResponseDto;
 import com.kwan.book.springboot.web.dto.PostsReponseDto;
 import com.kwan.book.springboot.web.dto.PostsSaveRequestDto;
 import com.kwan.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -22,13 +26,25 @@ public class PostsService {
 
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
-        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id =" +id));
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id =" + id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
 
-    public PostsReponseDto findById (Long id){
-        Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당사용자가 없습니다 id= " + id));
+    public PostsReponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당사용자가 없습니다 id= " + id));
         return new PostsReponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해달사용자가 없습니다. id=" + id));
+        postsRepository.delete(posts);
+
     }
 }
--- src/main/java/com/kwan/book/springboot/web/IndexController.java
+++ src/main/java/com/kwan/book/springboot/web/IndexController.java
@@ -1,20 +1,36 @@
 package com.kwan.book.springboot.web;
 
+import com.kwan.book.springboot.service.posts.PostsService;
+import com.kwan.book.springboot.web.dto.PostsReponseDto;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
     @GetMapping("/posts/save")
-    public String postsSave(){
+    public String postsSave() {
         return "posts-save";
     }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsReponseDto dto = postsService.findById(id);
+        model.addAttribute("posts", dto);
+        return "posts-update";
+    }
+
 }
